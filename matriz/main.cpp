68ea837f3485519d61ecd25e42d2729a54e4830b
--unset <--unset>
2026-03-10 15:35:56 -0300

    progress

--- matriz/main.cpp
+++ matriz/main.cpp
@@ -2,31 +2,99 @@
 
 using namespace std;
 
-
 // 1-) Criar uma matriz de ordem 4 e exibir a soma da diagonal principal.
 int exercicioA() {
-    int matriz[4][4] = {
-        {59, 12, 45, 12},
-        {6, 0, 81, 30},
-        {31, 14, 1, 26},
-        {92, 5, 1, 59},
-    };
-    int soma = 0;
+  int matriz[4][4] = {
+      {59, 12, 45, 12},
+      {6, 0, 81, 30},
+      {31, 14, 1, 26},
+      {92, 5, 1, 59},
+  };
+  int soma = 0;
 
-    for (int i=0; i<4; ++i) {
-        soma += matriz[i][i];
+  for (int i = 0; i < 4; ++i) {
+    soma += matriz[i][i];
+  }
+
+  cout << soma;
+
+  return 0;
+}
+// 2-) Ler valores inteiros para a matriz A[3][5]. Gerar e imprimir o vetor
+// Soma_Linha, onde cada elemento é a soma dos elementos de uma linha da matriz
+// A. Faça o trecho que gera a matriz separadamente da entrada e saída.
+int exercicioB() {
+  const int LINES = 3;
+  const int COLS = 5;
+  int matriz[LINES][COLS] = {0};
+  int soma_linha[LINES] = {0};
+
+  for (int i = 0; i < LINES; ++i) {
+    for (int j = 0; j < COLS; ++j) {
+      cout << "Digite o número na posição [" << i << "][" << j << "]: ";
+      cin >> matriz[i][j];
     }
+  }
 
-    cout << soma;
+  // Calcular vetor
+  for (int i = 0; i < LINES; ++i) {
+    for (int j = 0; j < COLS; ++j) {
+      soma_linha[i] += matriz[i][j];
+    }
+  }
 
-    return 0;
+  // Exibir vetor
+  cout << '{';
+  for (int i = 0; i < LINES; ++i) {
+    cout << soma_linha[i] << ", ";
+  }
+  cout << '}';
+
+  return 0;
 }
-// 2-) Ler valores inteiros para a matriz A[3][5]. Gerar e imprimir o vetor Soma_Linha, onde cada elemento é a soma dos elementos de uma linha da matriz A. Faça o trecho que gera a matriz separadamente da entrada e saída.
-// 3-) Criar uma matriz 2x3. Gerar e imprimir a transposta dessa matriz. A matriz transposta é gerada trocando linha por coluna.
-// 4-) Criar uma matriz de ordem 5 e imprima: toda a matriz e depois os números que se encontram em posições cuja a linha mais a coluna formam um número ímpar.
-// 5-) Ler os elementos de uma matriz de ordem 6 e imprima o produto dos elementos que estão abaixo da diagonal principal.
+// 3-) Criar uma matriz 2x3. Gerar e imprimir a transposta dessa matriz. A
+// matriz transposta é gerada trocando linha por coluna.
+int exercicioC() {
+  const int LINHAS = 2;
+  const int COLUNAS = 3;
 
+  int matriz[LINHAS][COLUNAS] = {0};
+  int transposta[COLUNAS][LINHAS];
 
-int main() {
-    return exercicioA();
+  for (int i = 0; i < LINHAS; ++i) {
+    for (int j = 0; j < COLUNAS; ++j) {
+      cout << "Digite o número na posição [" << i << "][" << j << "]: ";
+      cin >> matriz[i][j];
+    }
+  }
+
+  // CALCULAR TRANSPOSTA
+  for (int i = 0; i < LINHAS; ++i) {
+    for (int j = 0; j < COLUNAS; ++j) {
+      transposta[j][i] = matriz[i][j];
+    }
+  }
+
+  // Exibir Resultado
+  // obs: na transposta as colunas e linhas tão invertidas
+  cout << "{\n";
+  for (int i = 0; i < COLUNAS; ++i) {
+    cout << "{ ";
+    for (int j = 0; j < LINHAS; ++j) {
+      cout << transposta[i][j] << ' ';
+    }
+    cout << "}\n";
+  }
+  cout << "}";
+
+  return 0;
 }
+
+// 4-) Criar uma matriz de ordem 5 e imprima: toda a matriz e depois os números
+// que se encontram em posições cuja a linha mais a coluna formam um número
+// ímpar.
+
+// 5-) Ler os elementos de uma matriz de ordem 6 e imprima o produto dos
+// elementos que estão abaixo da diagonal principal.
+
+int main() { return exercicioC(); }
