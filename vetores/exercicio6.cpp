f69e6609d815d15b7d74add8f76441effad40de4
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-03-17 10:29:57 -0300

    partial: tentando implementar função partition)

--- vetores/exercicio6.cpp
+++ vetores/exercicio6.cpp
@@ -1,9 +1,9 @@
-#include <exception>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-template <typename T> void swap(T &a, T &b) {
+void swap(int &a, int &b) {
   int c = a;
   a = b;
   b = c;
@@ -11,10 +11,29 @@ template <typename T> void swap(T &a, T &b) {
 
 // Retorna o index do pivot no array
 int partition(int *arr, int start, int end) {
+  // TODO: código está quebrado, não sei porque. Arrumar
+  throw new exception;
   // Algorítmo de partição: utilizar dois ponteiros: um apontando a divisória
   // (j), outro iterando pelo array (i)
-  int i = 0, j = 0;
-  throw new exception;
+  // [22, 65, 7, 93, 4, 76, 29, 73, 89, 49]
+  int j = start - 1;
+  int pivot = arr[end];
+
+  for (int i = start; i <= end; ++i) {
+    // Evita os ponteiros de inverterem ou de uma troca ocorrer no mesmo
+    // elemento
+    if (i <= j + 1) {
+      continue;
+    }
+
+    if (arr[i] < pivot) {
+      swap(arr[++j], arr[i]);
+    }
+  }
+  std::vector<int> dbg_vec(&arr[start], &arr[end]);
+
+  // j agora está apontando para o pivot
+  return j;
 }
 
 void quicksort(int *arr, int start, int end) {
@@ -36,7 +55,6 @@ void quicksort(int *arr, int start, int end) {
 
 /*6-) Criar um vetor com a 8 elementos e ordená-los.*/
 int main() {
-  throw new exception;
   const int LEN = 8;
   int vetor[LEN] = {0};
 
@@ -46,4 +64,5 @@ int main() {
   }
 
   quicksort(vetor, 0, LEN - 1);
+  cout << vetor[0];
 }
